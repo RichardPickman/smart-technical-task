6c2de6b11ccb29f96a82d1eb890d6085dbc1e444
Richard Pickman <drezzerock@gmail.com>
2024-09-14 13:51:30 +0200

    update types and general layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,189 +1,112 @@
-import { AnimatePresence, AnimationProps, motion } from 'framer-motion';
-import { SearchIcon } from 'lucide-react';
-import { ChangeEvent, MouseEvent, useEffect, useMemo, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { ChangeEvent, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import { Button } from './components/ui/button';
 import { Input } from './components/ui/input';
 import {
     Table,
     TableBody,
-    TableCaption,
     TableCell,
     TableHead,
     TableHeader,
     TableRow,
 } from './components/ui/table';
-import { SearchState, setQuery, setSearchType } from './store/reducers/search';
+import { useFilteredUsers } from './hooks/useFilteredUsers';
+import { cn } from './lib/utils';
+import { TableKey, setValue } from './store/reducers/filters';
 import { fetchUsers } from './store/reducers/users';
 import { AppDispatch, RootState } from './store/store';
+import { User } from './types';
 
-const MotionInput = motion.create(Input);
-
-const visibleMotion: AnimationProps['animate'] = {
-    opacity: 1,
-    transform: 'translateY(0%)',
-    position: 'relative',
-};
-
-const invisibleMotion: AnimationProps['initial'] = {
-    opacity: 0,
-    transform: 'translateY(100%)',
-    position: 'absolute',
-};
-
-const TableHeadWithSearch = ({
-    inputType,
-}: {
-    inputType: SearchState['inputType'];
-}) => {
-    const [isInputVisible, setIsMenuVisible] = useState(false);
-    const { type, query } = useSelector((state: RootState) => ({
-        type: state.searchReducer.inputType,
-        query: state.searchReducer.query,
-    }));
+const TableHeadWithSearch = ({ field }: { field: TableKey }) => {
+    const values = useSelector(
+        (state: RootState) => state.filterReducer.values,
+    );
+    const query = values[field];
     const dispatch = useDispatch<AppDispatch>();
 
-    const setFloatMenu = (event: MouseEvent<HTMLButtonElement>) => {
-        const id = event.currentTarget.id as SearchState['inputType'];
-
-        dispatch(setSearchType(id));
-        setIsMenuVisible(true);
-    };
-
     const onChange = (e: ChangeEvent<HTMLInputElement>) =>
-        dispatch(setQuery(e.target.value));
-
-    const onBlur = () => setIsMenuVisible(false);
+        dispatch(setValue({ key: field, value: e.target.value }));
 
     return (
-        <TableHead className="spacing-2 w-52 overflow-hidden">
-            <div className="flex justify-between items-center overflow-hidden gap-2 p-2">
-                <AnimatePresence mode="sync">
-                    {!isInputVisible && (
-                        <motion.p
-                            key={inputType + '-label'}
-                            className="capitalize"
-                            initial={invisibleMotion}
-                            animate={!isInputVisible && visibleMotion}
-                            exit={invisibleMotion}
-                        >
-                            {inputType}
-                        </motion.p>
-                    )}
-                    {isInputVisible && type === inputType && (
-                        <MotionInput
-                            className="min-w-12 max-w-36 text-center border rounded"
-                            type="text"
-                            key={inputType + '-input'}
-                            placeholder="Search"
-                            value={query}
-                            onChange={onChange}
-                            onBlur={onBlur}
-                            initial={invisibleMotion}
-                            animate={
-                                isInputVisible &&
-                                type === inputType &&
-                                visibleMotion
-                            }
-                            exit={invisibleMotion}
-                        />
-                    )}
-                    <Button
-                        id={inputType}
-                        className="w-fit h-fit"
-                        onClick={setFloatMenu}
-                        variant="ghost"
-                    >
-                        <SearchIcon className="w-3 h-4" />
-                    </Button>
-                </AnimatePresence>
-            </div>
+        <TableHead className="m-0 p-0 divide-x-2">
+            <Input
+                className={cn(
+                    'w-full h-full m-0 rounded-none border-none',
+                    query ? 'bg-slate-200' : 'bg-background',
+                )}
+                type="text"
+                key={field + '-input'}
+                placeholder={field}
+                onChange={onChange}
+            />
         </TableHead>
     );
 };
 
 const MotionTableRow = motion.create(TableRow);
 
-const useFilteredUsers = () => {
-    const users = useSelector((state: RootState) => state.usersReducer.users);
-
-    const { inputType, query } = useSelector((state: RootState) => ({
-        inputType: state.searchReducer.inputType,
-        query: state.searchReducer.query,
-    }));
-
-    const filteredUsers = useMemo(() => {
-        return users.filter((item) => {
-            const data = item[inputType].toString().toLowerCase();
-
-            return data.includes(query.toLowerCase());
-        });
-    }, [query, users, inputType]);
-
-    return { filteredUsers };
-};
+const UserRow = ({ user }: { user: User }) => (
+    <MotionTableRow
+        key={user.id}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+    >
+        <TableCell>{user.id}</TableCell>
+        <TableCell>{user.name}</TableCell>
+        <TableCell>{user.username}</TableCell>
+        <TableCell>{user.email}</TableCell>
+        <TableCell>{user.phone}</TableCell>
+    </MotionTableRow>
+);
 
 const App = () => {
+    const users = useSelector((state: RootState) => state.usersReducer.users);
     const loading = useSelector(
         (state: RootState) => state.usersReducer.loading,
     );
-    const { filteredUsers: users } = useFilteredUsers();
+    const { users: filtered } = useFilteredUsers(users);
     const dispatch = useDispatch<AppDispatch>();
 
     useEffect(() => {
         dispatch(fetchUsers());
     }, [dispatch]);
 
-    const refetchUsers = () => dispatch(fetchUsers());
-
     return (
         <Table className="w-fit mx-auto border">
-            <TableCaption className="">
-                <p>Refetch data</p>
-                <Button onClick={refetchUsers}>Click</Button>
-            </TableCaption>
             <TableHeader>
                 <TableRow className="border rounded">
-                    <TableHeadWithSearch inputType="id" />
-                    <TableHeadWithSearch inputType="name" />
-                    <TableHeadWithSearch inputType="username" />
-                    <TableHeadWithSearch inputType="email" />
-                    <TableHeadWithSearch inputType="phone" />
+                    <TableHeadWithSearch field="id" />
+                    <TableHeadWithSearch field="name" />
+                    <TableHeadWithSearch field="username" />
+                    <TableHeadWithSearch field="email" />
+                    <TableHeadWithSearch field="phone" />
                 </TableRow>
             </TableHeader>
             <TableBody className="relative">
                 <AnimatePresence mode="sync">
                     {loading === 'succeeded' &&
-                        users.map((user) => (
-                            <MotionTableRow
-                                key={user.id}
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                            >
-                                <TableCell>{user.id}</TableCell>
-                                <TableCell>{user.name}</TableCell>
-                                <TableCell>{user.username}</TableCell>
-                                <TableCell>{user.email}</TableCell>
-                                <TableCell>{user.phone}</TableCell>
-                            </MotionTableRow>
+                        filtered.map((user) => (
+                            <UserRow key={user.id} user={user} />
                         ))}
+
+                    {loading === 'pending' && (
+                        <TableRow className="">
+                            <TableCell>Loading...</TableCell>
+                            <TableCell>Loading...</TableCell>
+                            <TableCell>Loading...</TableCell>
+                            <TableCell>Loading...</TableCell>
+                            <TableCell>Loading...</TableCell>
+                        </TableRow>
+                    )}
+
+                    {loading === 'failed' && (
+                        <TableRow className="">
+                            <TableCell colSpan={5}>Fetch failed.</TableCell>
+                        </TableRow>
+                    )}
                 </AnimatePresence>
-                {loading === 'pending' && (
-                    <TableRow className="">
-                        <TableCell>Loading...</TableCell>
-                        <TableCell>Loading...</TableCell>
-                        <TableCell>Loading...</TableCell>
-                        <TableCell>Loading...</TableCell>
-                        <TableCell>Loading...</TableCell>
-                    </TableRow>
-                )}
-                {loading === 'failed' && (
-                    <TableRow className="">
-                        <TableCell colSpan={5}>Fetch failed.</TableCell>
-                    </TableRow>
-                )}
             </TableBody>
         </Table>
     );
