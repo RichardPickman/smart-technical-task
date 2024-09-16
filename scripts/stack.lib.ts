17cba887cd85dfc1750a95d92dae835f875dc6ec
Richard Pickman <drezzerock@gmail.com>
2024-09-16 17:16:42 +0200

    update projects

---
+++ scripts/stack.lib.ts
@@ -0,0 +1,43 @@
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { Distribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { Construct } from 'constructs';
+import * as path from 'path';
+
+export class StaticSiteStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const bucket = new Bucket(this, 'MyStaticSiteBucket', {
+            websiteIndexDocument: 'index.html',
+            websiteErrorDocument: 'index.html',
+            publicReadAccess: true,
+            blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+        });
+
+        new BucketDeployment(this, 'BucketDeployment', {
+            destinationBucket: bucket,
+            sources: [Source.asset(path.resolve(__dirname, './dist'))],
+        });
+
+        const originAccessIdentity = new OriginAccessIdentity(
+            this,
+            'OriginAccessIdentity',
+        );
+        bucket.grantRead(originAccessIdentity);
+
+        const distribution = new Distribution(this, 'Distribution', {
+            defaultRootObject: 'index.html',
+            defaultBehavior: {
+                origin: S3BucketOrigin.withOriginAccessIdentity(bucket),
+            },
+        });
+
+        new CfnOutput(this, 'bucket', { value: bucket.bucketName });
+        new CfnOutput(this, 'distribution', {
+            value: distribution.distributionDomainName,
+        });
+    }
+}
